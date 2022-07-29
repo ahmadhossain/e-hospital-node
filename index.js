d5d509ac23f1e699d929106832784ffa64a66b7e
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2022-07-30 00:55:23 +0600

    registration route added

--- index.js
+++ index.js
@@ -8,9 +8,18 @@ import dotenv from 'dotenv'
 const app = express();
 dotenv.config()
 
+const users = [];
+
 app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
+
+app.post('/users',(req, res)=>{ 
+    const user =  { name: req.body.name, password: req.body.password }
+    users.push(user)
+    res.status(201).send("Created")
+
+})
 
 const MONGODB_URL = process.env.MONGODB_URL;
 const PORT = process.env.PORT || 5000;
@@ -18,4 +27,5 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(MONGODB_URL)
     .then(()=>app.listen(PORT,()=> console.log(`Server running on port: ${PORT}`)))
     .catch((err)=>console.log(err));
-    
+
+
