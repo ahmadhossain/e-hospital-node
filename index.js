d5b3030db241e93a0d9b6fd7e938cf96e98b3816
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2022-08-13 18:30:03 +0600

    -Schema created -Register Route created

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv'
-
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import Doctor from "./models/doctor.js";
+import Paitent from "./models/patient.js";
 
 const app = express();
-dotenv.config()
-
-const users = [];
-
-app.use(bodyParser.json({extended: true}));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
-
-app.post('/users',(req, res)=>{ 
-    const user =  { name: req.body.name, password: req.body.password }
-    users.push(user)
-    res.status(201).send("Created")
-
-})
+dotenv.config();
 
 const MONGODB_URL = process.env.MONGODB_URL;
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(MONGODB_URL)
-    .then(()=>app.listen(PORT,()=> console.log(`Server running on port: ${PORT}`)))
-    .catch((err)=>console.log(err));
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
+// register a patient
+app.post("/patient", (req, res) => {
+  const paitent = new Paitent(req.body);
+  console.log(paitent);
+  paitent.save().then((data) => {
+    res.status(201).send("Created");
+  });
+});
+// register a doctor
+app.post("/doctor", (req, res) => {
+  const doctor = new Doctor(req.body);
+  console.log(doctor);
+  doctor.save().then((data) => {
+    res.status(201).send("Created");
+  });
+});
 
+mongoose
+  .connect(MONGODB_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((err) => console.log(err));
---
+++ models/doctor.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const DoctorSchema = new mongoose.Schema({
+  FullName: String,
+  Email: String,
+  Age: String,
+  Education: String,
+  Result: String,
+  Password: String,
+  Role: String,
+});
+
+export default mongoose.model("Doctor", DoctorSchema);
